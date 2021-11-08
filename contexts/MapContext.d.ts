1554cfac59e490ea1ed784b11214d1a2c3001b82
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2021-11-08 22:27:54 +1300

    Updates

--- contexts/MapContext.d.ts
+++ contexts/MapContext.d.ts
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl';
+import React from "react";
+import mapboxgl from "mapbox-gl";
 export interface IMapContext {
     map?: mapboxgl.Map;
     setMap: (map: mapboxgl.Map) => void;
     resetMap: () => void;
 }
 export declare const MapContext: React.Context<IMapContext>;
-export declare class MapProvider extends Component {
-    state: {
-        map?: mapboxgl.Map;
-    };
-    setMap(map: mapboxgl.Map): void;
-    render(): JSX.Element;
-}
+export declare const MapProvider: React.FC;
