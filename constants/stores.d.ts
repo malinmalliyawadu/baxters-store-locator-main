3115e4b78429e65e25c73f5aff253944888d3c9a
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2021-12-02 20:21:34 +1300

    Updates

--- constants/stores.d.ts
+++ constants/stores.d.ts
@@ -1,11 +1,11 @@
 export declare type Region = "Northland" | "Waikato" | "Auckland North" | "Auckland West" | "Auckland Central" | "Auckland East" | "Auckland South" | "Hamilton" | "Bay of Plenty" | "Taranaki" | "Whanganui" | "Manawatu" | "Marlborough" | "Wairarapa" | "Wellington" | "Nelson" | "Tasman" | "Canterbury" | "Otago";
 export interface IStoreDetails {
     name: string;
-    lngLat: [number, number];
-    address: string;
-    locality: string;
-    postcode: string;
-    region: Region;
+    lngLat?: [number, number];
+    address?: string;
+    locality?: string;
+    postcode?: string;
+    region?: Region;
     phone?: string;
     storeBrand?: string;
     storeType: string;
